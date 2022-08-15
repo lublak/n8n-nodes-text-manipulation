31f270d9bc57f46269d8574a0e8fba65eff9e355
538023 <lublak.de@gmail.com>
2022-08-15 18:39:09 +0200

    move nodes to seperate folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,3 @@
 module.exports = {
-	env: {
-		browser: true,
-		es6: true,
-		node: true,
-	},
-	extends: ['eslint-config-n8n-nodes-base'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: ['./tsconfig.json'],
-		sourceType: 'module',
-		extraFileExtensions: ['.json'],
-	},
-	ignorePatterns: ['.eslintrc.js', '**/*.js', '**/node_modules/**', '**/dist/**'],
-
-	settings: {
-		jsdoc: {
-			mode: 'typescript',
-			structuredTags: {
-				type: {
-					type: true,
-					required: ['type'],
-				},
-			},
-			ignoreInternal: true,
-		},
-	}
+	extends: ['eslint-config-n8n-nodes-base']
 };
